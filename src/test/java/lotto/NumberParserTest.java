9a448ded972f7099d231241393e00b6c8b9e9995
shoojoy <157701033+shoojoy@users.noreply.github.com>
2025-10-31 23:32:45 +0900

    feat(test): NumberParser 테스트 완료

--- src/test/java/lotto/NumberParserTest.java
+++ src/test/java/lotto/NumberParserTest.java
@@ -1,4 +1,28 @@
 package lotto;
 
-public class NumberParserTest {
+import lotto.util.NumberParser;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import static org.assertj.core.api.Assertions.*;
+
+class NumberParserTest {
+    @Test
+    void csv를_정수_리스트로_파싱() {
+        List<Integer> nums = NumberParser.parseCsvToIntegers("1, 2,3, 4,5,6");
+        assertThat(nums).containsExactly(1,2,3,4,5,6);
+    }
+
+    @Test
+    void 빈토큰이면_예외() {
+        assertThatThrownBy(() -> NumberParser.parseCsvToIntegers("1,2,,4,5,6"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith("[ERROR]");
+    }
+
+    @Test
+    void 숫자아니면_예외() {
+        assertThatThrownBy(() -> NumberParser.parseCsvToIntegers("1,2,a,4,5,6"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith("[ERROR]");
+    }
 }
