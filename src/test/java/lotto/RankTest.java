96996e1e83e0570ec14cb7aa26288f0cb39b2444
shoojoy <157701033+shoojoy@users.noreply.github.com>
2025-10-31 23:45:11 +0900

    test(domain): Rank 등수 판정 파라미터라이즈드 테스트 추가

--- src/test/java/lotto/RankTest.java
+++ src/test/java/lotto/RankTest.java
@@ -1,4 +1,24 @@
 package lotto;
 
-public class RankTest {
+import lotto.domain.Rank;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RankTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "6,false,FIRST",
+            "5,true,SECOND",
+            "5,false,THIRD",
+            "4,false,FOURTH",
+            "3,false,FIFTH",
+            "2,false,MISS",
+            "0,false,MISS"
+    })
+    void 등수_판정(int match, boolean bonus, Rank expected) {
+        assertThat(Rank.of(match, bonus)).isEqualTo(expected);
+    }
 }
