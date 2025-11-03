62f1c38ac9149ffacd60ef719dedf1c0f5fd096b
shoojoy <1you1213@naver.com>
2025-11-03 19:11:30 +0900

    fix(lotto-issuer): Lotto.random() 사용하도록 발행 로직 수정

--- src/main/java/lotto/domain/LottoIssuer.java
+++ src/main/java/lotto/domain/LottoIssuer.java
@@ -1,7 +1,5 @@
 package lotto.domain;
 
-import camp.nextstep.edu.missionutils.Randoms;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,9 +7,7 @@ public class LottoIssuer {
     public List<Lotto> issue(int count) {
         List<Lotto> tickets = new ArrayList<>();
         for (int i = 0; i < count; i++) {
-            tickets.add(new Lotto(
-                    Randoms.pickUniqueNumbersInRange(Lotto.MIN, Lotto.MAX, Lotto.COUNT)
-            ));
+            tickets.add(Lotto.random());
         }
         return tickets;
     }
